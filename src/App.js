45b9dedd73ea5805e6bf32850e9ae844fe09c519
wyd <w_yidan@163.com>
2019-12-20 16:33:55 +0800

    react_echarts_demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/bar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class indexBarData extends React.Component{
+   initCharts = ()=>{
+    var option = {
+      dataset: {
+          source: [
+              ['number', 'name'],
+              [119, '呼损量'],
+              [307, '取号未呼量'],
+              [389, '漏接量'],
+              [151, '业务办理量'],
+              [455, '接触量'],
+              [667,  '接通量'],
+              [940, '外呼量'],
+          ]
+      },
+      grid: {containLabel: true},
+      xAxis:{
+          type: 'value',
+          axisTick: {
+              show: false
+          },
+          axisLine: {
+              lineStyle: {
+                  color: '#666'
+              }
+          },
+          interval: 100
+      },
+      yAxis: {
+          type: 'category',
+          axisTick: {
+              show: false
+          },
+          axisLine: {
+              show: false
+          }
+      },
+      series: [
+          {
+              type: 'bar',
+              barMaxWidth: 13,
+              itemStyle: {
+                  color: '#9dcce4',
+                  barBorderRadius: 20,
+                  },
+              label:  {
+                  show: true,
+                  position: 'right',
+                  color: '#000'
+              },
+              encode: {
+                  // Map the "amount" column to X axis.
+                  x: 'number',
+                  // Map the "product" column to Y axis
+                  y: 'name'
+              }
+          }
+      ]
+  };
+  echarts.init(document.getElementById('main')).setOption(option);
+  }
+
+  componentDidMount(){
+    this.initCharts()
+  }
+
+  componentDidUpdate(){
+    this.initCharts()
+  }
+
+  render(){
+    return (
+      <div id='main' style = {{height: 400, width: 700}}></div>
+    )
+  }
 }
-
-export default App;
